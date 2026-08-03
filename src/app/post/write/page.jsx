6d975843606909f01d67b6654a701bd01b7c7caf
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-03 14:27:59 +0900

    게시판 작성 페이지 레이아웃 구성 및 스타일 시트 추가

--- src/app/post/write/page.jsx
+++ src/app/post/write/page.jsx
@@ -1,1 +1,10 @@
 // [게시판 작성] 페이지 (localhost:3000/post/write)
+import "./style.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
