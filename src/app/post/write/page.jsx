01c66d1305fe318b72138181ffc2bbb153d5fe38
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-03 22:10:10 +0900

    fix: import 경로를 절대 경로로 수정

--- src/app/post/write/page.jsx
+++ src/app/post/write/page.jsx
@@ -1,5 +1,5 @@
 // [게시판 작성] 페이지 (localhost:3000/post/write)
-import WriteForm from "../../../components/post/write/WriteForm.jsx";
+import WriteForm from "@/components/post/write/WriteForm.jsx";
 
 export default function PostWritePage() {
   return (
