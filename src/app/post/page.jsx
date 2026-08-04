2da11aaab8c37d1a9f0c0e20d2ccb4305ba77f22
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-04 15:21:56 +0900

    style: 페이지 레이아웃에 클래스 추가 및 CSS 파일 임포트

--- src/app/post/page.jsx
+++ src/app/post/page.jsx
@@ -1,3 +1,6 @@
+import "@/community/common.css";
+import "@/community/post.css";
+
 // [게시판 목록] 페이지 (localhost:3000/post)
 import PostFilter from "@/components/post/list/PostFilter";
 import PostItem from "@/components/post/list/PostItem";
@@ -5,7 +8,7 @@ import TopThree from "@/components/post/list/TopThree";
 
 export default function PostListPage() {
   return (
-    <main>
+    <main className="community-scope community-page post-list-page">
       {/* 상단 TOP 3 게시글 영역 */}
       <TopThree />
 
