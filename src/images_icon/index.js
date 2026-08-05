306fa95eb36219eacf1cc378818f5b8e07906aae
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-05 20:36:18 +0900

    fix: DeleteOutline 아이콘의 경로를 수정하여 올바른 아이콘으로 변경

--- src/images_icon/index.js
+++ src/images_icon/index.js
@@ -3,7 +3,7 @@ export { default as AccountCircleIcon } from "@mui/icons-material/AccountCircle"
 export { default as AutoAwesomeIcon } from "@mui/icons-material/AutoAwesome";
 export { default as ChatBubbleOutlineIcon } from "@mui/icons-material/ChatBubbleOutlineOutlined";
 export { default as CloseIcon } from "@mui/icons-material/Close";
-export { default as DeleteOutlineIcon } from "@mui/icons-material/DeleteOutline";
+export { default as DeleteOutlineIcon } from "@mui/icons-material/DeleteOutlined";
 export { default as EmojiEventsIcon } from "@mui/icons-material/EmojiEvents";
 export { default as FavoriteIcon } from "@mui/icons-material/Favorite";
 export { default as FavoriteBorderIcon } from "@mui/icons-material/FavoriteBorder";
