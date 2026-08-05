38b2fbbbdae98e6dd366bdca040bde807944c2be
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-05 19:46:38 +0900

    feat: MUI React SVG 아이콘을 관리하는 index.js 파일 추가

---
+++ src/images_icon/index.js
@@ -0,0 +1,15 @@
+// [커뮤니티 공통 아이콘] MUI React SVG 아이콘을 한 곳에서 관리합니다.
+export { default as AccountCircleIcon } from "@mui/icons-material/AccountCircle";
+export { default as AutoAwesomeIcon } from "@mui/icons-material/AutoAwesome";
+export { default as ChatBubbleOutlineIcon } from "@mui/icons-material/ChatBubbleOutline";
+export { default as CloseIcon } from "@mui/icons-material/Close";
+export { default as DeleteOutlineIcon } from "@mui/icons-material/DeleteOutline";
+export { default as EmojiEventsIcon } from "@mui/icons-material/EmojiEvents";
+export { default as FavoriteIcon } from "@mui/icons-material/Favorite";
+export { default as FavoriteBorderIcon } from "@mui/icons-material/FavoriteBorder";
+export { default as FolderOpenIcon } from "@mui/icons-material/FolderOpen";
+export { default as InfoOutlinedIcon } from "@mui/icons-material/InfoOutlined";
+export { default as LocalFireDepartmentIcon } from "@mui/icons-material/LocalFireDepartment";
+export { default as PushPinIcon } from "@mui/icons-material/PushPin";
+export { default as RefreshIcon } from "@mui/icons-material/Refresh";
+export { default as RemoveRedEyeIcon } from "@mui/icons-material/RemoveRedEye";
