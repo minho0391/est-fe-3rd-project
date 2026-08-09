2333bd23d84edbcd2b4d63eeeeab4958678040b1
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-09 19:21:32 +0900

    feat: 커뮤니티 아이콘 추가 및 관리

--- src/images/icons/index.js
+++ src/images/icons/index.js
@@ -13,3 +13,13 @@ export { default as LocalFireDepartmentIcon } from "@mui/icons-material/LocalFir
 export { default as PushPinIcon } from "@mui/icons-material/PushPin";
 export { default as RefreshIcon } from "@mui/icons-material/Refresh";
 export { default as RemoveRedEyeIcon } from "@mui/icons-material/RemoveRedEye";
+
+export { default as BookmarkBorderIcon } from "@mui/icons-material/BookmarkBorder";
+export { default as ChevronLeftIcon } from "@mui/icons-material/ChevronLeft";
+export { default as ChevronRightIcon } from "@mui/icons-material/ChevronRight";
+export { default as ContentCopyIcon } from "@mui/icons-material/ContentCopy";
+export { default as ForumOutlinedIcon } from "@mui/icons-material/ForumOutlined";
+export { default as HomeOutlinedIcon } from "@mui/icons-material/HomeOutlined";
+export { default as LogoutIcon } from "@mui/icons-material/Logout";
+export { default as ManageAccountsOutlinedIcon } from "@mui/icons-material/ManageAccountsOutlined";
+export { default as ShareOutlinedIcon } from "@mui/icons-material/ShareOutlined";
